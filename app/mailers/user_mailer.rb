d2b5502f993f1562bd553655771ef551c02a16c4
Vincent Simon <vincent.simon@gadz.org>
2017-04-17 22:09:58 +0200

    ok

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def welcome(id)
     @user = User.find(id)
 
-    mail to:@user.email, subject:"we need you bro!"
+    mail to:@user.email, subject:"Etude sur l'Avocat 2.0"
   end
 
 end
